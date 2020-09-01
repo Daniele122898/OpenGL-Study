d41b19fd9acb6be74d76894fa1018227ac954b43
Serenity <d.palbanese@gmail.com>
2020-09-01 16:50:54 +0200

    Added keyboard and mouse support

--- OpenGLStudy/GLWindow.cpp
+++ OpenGLStudy/GLWindow.cpp
@@ -1,5 +1,6 @@
 #include "GLWindow.h"
 
+
 GLWindow::GLWindow()
 {
 	width = 800;
@@ -7,6 +8,15 @@ GLWindow::GLWindow()
 	bufferWidth = 0;
 	bufferHeight = 0;
 	mainWindow = nullptr;
+
+	lastX = 0;
+	lastY = 0;
+	xChange = 0;
+	yChange = 0;
+
+	mouseFirstMoved = false;
+
+	memset(keys, 0, sizeof(bool) * 1024);
 }
 
 GLWindow::GLWindow(GLint windowWidth, GLint windowHeight)
@@ -16,6 +26,15 @@ GLWindow::GLWindow(GLint windowWidth, GLint windowHeight)
 	bufferWidth = 0;
 	bufferHeight = 0;
 	mainWindow = nullptr;
+
+	lastX = 0;
+	lastY = 0;
+	xChange = 0;
+	yChange = 0;
+	
+	mouseFirstMoved = false;
+
+	memset(keys, 0, sizeof(bool) * 1024);
 }
 
 int GLWindow::Initialise()
@@ -49,6 +68,11 @@ int GLWindow::Initialise()
 	// set context for GLEW to use (incase of multiple windows)
 	glfwMakeContextCurrent(mainWindow);
 
+	// Handle key and mouse inputs
+	createCallbacks();
+	// To capture mouse in game
+	glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	
 	// Allow modern extension features
 	glewExperimental = GL_TRUE;
 
@@ -66,11 +90,74 @@ int GLWindow::Initialise()
 
 	// Setup viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight); // set width and height to the actual size inside the window we got when getting the buffer sizes
+
+	glfwSetWindowUserPointer(mainWindow, this); // So the window has a pointer to this class
+	
 	return 0;
+}
+
+GLfloat GLWindow::GetXChange()
+{
+	GLfloat change = xChange;
+	xChange = 0;
+	return change;
+}
+
+GLfloat GLWindow::GetYChange()
+{
+	GLfloat change = yChange;
+	yChange = 0;
+	return change;
 }
 
 GLWindow::~GLWindow()
 {
 	glfwDestroyWindow(mainWindow);
 	glfwTerminate();
+}
+
+void GLWindow::createCallbacks()
+{
+	glfwSetKeyCallback(mainWindow, handleKeys);
+	glfwSetCursorPosCallback(mainWindow, handleMouse);
+}
+
+void GLWindow::handleKeys(GLFWwindow* window, int key, int code, int action, int mode)
+{
+	GLWindow* ourWindow = static_cast<GLWindow*>(glfwGetWindowUserPointer(window));
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if (key >= 0 && key < 1024)
+	{
+		if(action == GLFW_PRESS)
+		{
+			ourWindow->keys[key] = true;
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			ourWindow->keys[key] = false;
+		}
+	}
+}
+
+void GLWindow::handleMouse(GLFWwindow* window, double xPos, double yPos)
+{
+	GLWindow* ourWindow = static_cast<GLWindow*>(glfwGetWindowUserPointer(window));
+
+	if (!ourWindow->mouseFirstMoved)
+	{
+		ourWindow->lastX = xPos;
+		ourWindow->lastY = yPos;
+		ourWindow->mouseFirstMoved = true;
+	}
+
+	ourWindow->xChange = xPos - ourWindow->lastX;
+	ourWindow->yChange = ourWindow->lastY - yPos; // Flipped to not have inverted controls.
+
+	ourWindow->lastX = xPos;
+	ourWindow->lastY = yPos;
 }
--- OpenGLStudy/GLWindow.h
+++ OpenGLStudy/GLWindow.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstdio>
+#include <cstring>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -17,6 +19,13 @@ public:
 
 	bool GetShouldClose() const { return glfwWindowShouldClose(mainWindow); }
 
+	// TODO don't do this in future...
+	bool* GetKeys() { return keys; }
+
+	// TODO change this in future as well
+	GLfloat GetXChange();
+	GLfloat GetYChange();
+
 	void SwapBuffers() { glfwSwapBuffers(mainWindow); }
 	
 	~GLWindow();
@@ -26,5 +35,18 @@ private:
 
 	GLint width, height;
 	GLint bufferWidth, bufferHeight;
+
+	bool keys[1024];
+
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	bool mouseFirstMoved;
+
+	void createCallbacks();
+
+	static void handleKeys(GLFWwindow* window, int key, int code, int action, int mode);
+	static void handleMouse(GLFWwindow* window, double xPos, double yPos);
 };
 
